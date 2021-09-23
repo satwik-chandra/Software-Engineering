842f128a7cb8d6a0e9302208e9af6dc0e0fe8f7d
satwik-chandra <chandrs1@tcd.ie>
2021-09-23 14:41:31 +0100

    New functional Code

--- LAC/src/App.java
+++ LAC/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LowestCommonAncestor tree = new LowestCommonAncestor();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+ 
+        System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
+        System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
+        System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
+        System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
+     
     }
 }
