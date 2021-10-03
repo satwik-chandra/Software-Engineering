8e1a91816378b69105ce15d0c033c8ed62e69070
satwik-chandra <chandrs1@tcd.ie>
2021-10-03 17:33:06 +0100

    Made some small changes, added a test class

---
+++ LAC/src/LowestCommonAncestorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Satwik Chandra
+ *
+ */
+public class LowestCommonAncestorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- LAC/src/MainTest.java
+++ LAC/src/MainTest.java
@@ -1,4 +1,6 @@
-public class App {
+
+
+public class MainTest {
     public static void main(String[] args) throws Exception {
         LowestCommonAncestor tree = new LowestCommonAncestor();
         tree.root = new Node(1);
@@ -8,11 +10,16 @@ public class App {
         tree.root.left.right = new Node(5);
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
+        tree.root.left.left.left = new Node(8);
+        tree.root.left.left.right = new Node(9);
  
         System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
         System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
         System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
         System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
+        System.out.println("LCA(8, 9): " + tree.findLCA(8,9));
+        System.out.println("LCA(0, 0): " + tree.findLCA(0,0));
      
     }
 }
+ 
